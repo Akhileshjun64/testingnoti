53b44c17ffd9c157e41d0e672ef0029572df7984
akhileshjune64 <akhileshjun64@gmail.com>
2023-12-05 13:49:00 +0530

    db added

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect("mongodb://0.0.0.0:27017/testingdec");
+    // await mongoose.connect("mongodb://0.0.0.0:27017/testingdec");
+    await mongoose.connect(
+      "mongodb+srv://akhileshjun64:DQl7sqU8kpKy3QkO@testingnotidb.4xqjoqq.mongodb.net/testnotidb"
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
